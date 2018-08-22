ed3f1ff19e8e4427b9bf3fe1478e21a0850abc37
Paulo Silva <paulosilvadev3@gmail.com>
2018-08-22 14:45:01 -0300

    test to report service

--- tests/test_app.py
+++ tests/test_app.py
@@ -6,10 +6,22 @@ class TestReportService:
     """
     Class to Test APP
     """
+
     def test_index(self, client):
         res = client.get(url_for('index'))
         assert res.status_code == 200
         assert res.json == {
             'title': 'Report Service',
             'version': '1.0.0'
+        }
+
+    def test_report(self, client):
+        res = client.get(url_for('report', city='São Luís', uf='MA', company='5b74e44d6906800036631800'))
+        assert res.status_code == 200
+        assert res.json == {
+            'city': 'São Luís'.encode('utf8'),
+            'uf': 'MA',
+            'company_id': '5b74e44d6906800036631800',
+            'company_name': 'CEMAR - Companhia Elétrica'.encode('utf8'),
+            'total_complains': 1
         }
