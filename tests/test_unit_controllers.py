3eb2a8ac99a557b4a8e5bad2e598d6eccb10aa36
Paulo Silva <paulosilvadev3@gmail.com>
2018-08-23 23:07:35 -0300

    add comments

--- tests/test_unit_controllers.py
+++ tests/test_unit_controllers.py
@@ -8,13 +8,19 @@ from mock import Mock
 from report import controllers
 
 class TestReportController():
+    """
+    Class to build a tests unit to Report Controller
+    """
     
     def test_build_report(self):
-        mockdb = Mock()
+        """
+        Test method build report
+        """
+        mockdb = Mock() #Build mock to DB call
         input_expected = {
             'city':'São Luís'.encode('utf8'), 
             'uf':'MA',
             'company':'5b74e44d6906800036631800'
-        }
+        } # Expected call input
         controllers.ReportController.build_report(controllers.ReportController, input_expected, mockdb)
         mockdb.find_report.assert_called_with(input_expected)
