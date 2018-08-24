eea2f31b0b3410891e3f8f7c12e00963a1d22895
Paulo Silva <paulosilvadev3@gmail.com>
2018-08-23 22:55:41 -0300

    fix dependencies

--- setup.py
+++ setup.py
@@ -1,15 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from setuptools import setup
 
 requirements = [
     'aniso8601',
+    'apipkg',
+    'astroid',
+    'atomicwrites',
+    'attrs',
     'click',
+    'coverage',
+    'execnet',
     'Flask',
+    'isort',
     'itsdangerous',
     'Jinja2',
+    'lazy-object-proxy',
     'MarkupSafe',
+    'mccabe',
+    'mock',
+    'more-itertools',
+    'pathlib2',
+    'pbr',
+    'pep8',
+    'pluggy',
+    'py',
+    'pymongo',
     'pytz',
     'six',
-    'Werkzeug'
+    'typed-ast',
+    'Werkzeug',
+    'wrapt',
 ]
 
 setup(
@@ -25,7 +47,9 @@ setup(
             'pytest-pep8',
             'pytest-cov',
             'pytest-flask',
-            'pytest-cache'
+            'pytest-cache',
+            'pytest',
+            'pylint',
         ]
     }
 )
