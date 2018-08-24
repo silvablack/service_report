4b1b6bd99935808c0594c40af73efc9e86e9af8f
Paulo Silva <paulosilvadev3@gmail.com>
2018-08-23 23:03:23 -0300

    rename test integration and add comments

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import os, pytest
 
 from context import report
 
-@pytest.fixture
+@pytest.fixture()
 def app():
     app = report.service(os.environ.get('API_ENV'))
     app.debug=True
--- tests/test_integration_app.py
+++ tests/test_integration_app.py
@@ -1,13 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import pytest
 
 from flask import url_for
 
 class TestReportService:
     """
-    Class to Test APP
+    Class to Test Integration with Report Service
     """
 
     def test_index(self, client):
+        """
+        Test default route /
+        Should return title and version
+        """
         res = client.get(url_for('index'))
         assert res.status_code == 200
         assert res.json == {
@@ -16,6 +23,10 @@ class TestReportService:
         }
 
     def test_report(self, client):
+        """
+        Test report route /report
+        Should return list in json object that contains city, uf, company_id, company_name, total_complains
+        """
         res = client.get(url_for('report', city='São Luís', uf='MA', company='5b74e44d6906800036631800'))
         assert res.status_code == 200
         assert res.json == {
