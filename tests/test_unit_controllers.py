f924d5c13a2a9fb749cd323a4c1970ae3077d515
Paulo Silva <paulosilvadev3@gmail.com>
2018-08-26 12:26:28 -0300

    fix test unit - report controller

--- tests/test_unit_controllers.py
+++ tests/test_unit_controllers.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import unittest
 import pytest
 from mock import Mock
 
@@ -16,11 +15,11 @@ class TestReportController():
         """
         Test method build report
         """
-        mockdb = Mock() #Build mock to DB call
+        mock = Mock() #mock for call
         input_expected = {
             'city':'São Luís'.encode('utf8'), 
             'uf':'MA',
             'company':'5b74e44d6906800036631800'
-        } # Expected call input
-        controllers.ReportController.build_report(controllers.ReportController, input_expected, mockdb)
-        mockdb.find_report.assert_called_with(input_expected)
+        } # Expected data input
+        controllers.ReportController.build_report(input_expected, mock)
+        mock.find_report.assert_called_with(input_expected)
