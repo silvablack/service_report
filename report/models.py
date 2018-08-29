a476c81ea82273d8d65ef839f698b0be25b4f5d0
Paulo Silva <paulosilvadev3@gmail.com>
2018-08-28 22:46:27 -0300

    fix response to no data company  and add comments

--- report/models.py
+++ report/models.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import os, json
-from bson.dbref import DBRef
 from bson.objectid import ObjectId
 from mongoengine import Document, EmbeddedDocument, fields
 from mongoengine.context_managers import switch_db
 from report.db import ConnectCompany, ConnectComplains
 
 class Companies(Document):
+    """
+    Schema to Document for Companies
+    """
     name = fields.StringField(required=True)
     mail = fields.EmailField()
     cnpj = fields.StringField()
@@ -15,11 +17,17 @@ class Companies(Document):
     meta = {'Companies': 'db_company'}
 
 class Locale(EmbeddedDocument):
+    """
+    Schema to Embedded Document for Locale in Complains
+    """
     city = fields.StringField(required=True)
     uf = fields.StringField(required=True)
     state = fields.StringField(required=True)
 
 class Complains(Document):
+    """
+    Schema to Document for Complais
+    """
     title = fields.StringField(required=True)
     description = fields.StringField(required=True)
     locale = fields.EmbeddedDocumentField(Locale)
@@ -31,27 +39,36 @@ class Complains(Document):
 
 
 class SearchComplainsByCity(object):
+    """
+    Class to Search Complains By City
+    Builds the report from the parameters
+    """
 
     def find_report(objs):
-        ConnectComplains()
-        ConnectCompany()
+        ConnectComplains() #register complains connection
+        ConnectCompany() #register company connection
 
         with switch_db(Companies, 'db_company') as Ca:
             company = Ca.objects(id=ObjectId(objs['company']))
-
+        
         with switch_db(Complains, 'default') as Co:
             complains = Co.objects.filter(company_id=objs['company'],locale__city=objs['city'], locale__uf=objs['uf']).all()
         
-        for x in company:
-            company_name = x.name
-
-        data = {
-            'company': company_name,
-            'city': objs['city'],
-            'uf': objs['uf'],
-            'company_id': objs['company'],
-            'total_complains': complains.count()
-        }
+        if(len(company)==1): # CHECK IF COMPANY EXISTS
+            for x in company:
+                company_name = x.name
+            # build data json
+            data = {
+                'company': company_name,
+                'city': objs['city'],
+                'uf': objs['uf'],
+                'company_id': objs['company'],
+                'total_complains': complains.count()
+            }
+        else:
+            data = {
+                'error': 'NO DATA COMPANY'
+            }    
 
         return json.dumps(data)
 
